b77da116b6a3d07b353a75cfd596485335978f41
ErdemSusam23 <ersusam2003@gmail.com>
2023-05-29 15:19:44 +0300

    ui improvements

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -9,7 +13,7 @@ using UnityEngine;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
-
+    
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
@@ -21,4 +25,20 @@ public class MenuUIHandler : MonoBehaviour
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
     }
+
+    private void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); 
+#endif
+    }
+
+
 }

aa92b51562356bb232a7f90c63077ff3c8ecad9c
ErdemSusam23 <ersusam2003@gmail.com>
2023-06-30 14:02:03 +0300

    update productivityUnit script

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    private ResourcePile m_CurrentPile;
+    public float productivityMultiplier = 2f;
+
+
     protected override void BuildingInRange()
     {
-        throw new System.NotImplementedException(); 
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= productivityMultiplier;
+            }
+        }
+        
     }
 }
